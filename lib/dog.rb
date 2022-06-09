e0a3fab328ce897771658fb6e63a73ca404976b1
MissDine <enidnyatichi1212@gmail.com>
2022-06-09 23:52:26 +0300

    Feature edit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,10 @@
 class Dog
+    def bark
+        puts "Woof!"
+    end 
+    def sit
+        puts "The Dog is sitting"
+    end   
 end
 
 # Creating a new instance/object of the class Dog
@@ -9,3 +15,13 @@ puts fido.object_id
 # fido is the receiver and object_id is the message
 # We can ask an object or class instance what methods it responds to by .methods
 puts fido.methods
+# How to add methods to our own class-by placing this method definition within the body of a class
+# the method becomes a specific behavior of instances of that class, not a generic procedure we can just call whenever we want.
+# We call the methods defined within the object's class Instance Methods because they are methods that belong to any instance of the class.
+fido.bark
+fido.sit
+
+
+snoopy = Dog.new
+snoopy.bark
+snoopy.sit
