29ec62164c321768e6045c2461a83c07d97a779a
MissDine <enidnyatichi1212@gmail.com>
2022-06-09 23:55:06 +0300

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,4 @@
-# Creating a class and its various behaviours or  instance methods
+# Creating a class and its various behaviour or  instance methods
 class Person
     def talk
         puts "Hello World!"
